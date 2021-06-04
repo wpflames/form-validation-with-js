e3c85d09141e298860c9287f58e3fbffdac96962
Gabor Flamich <70385943+wpflames@users.noreply.github.com>
2021-06-04 19:30:33 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+    const name = document.getElementById('name');
+    const re = /^[a-zA-Z\s]{2,20}$/;
+
+    if(!re.test(name.value)){
+        name.classList.add('is-invalid');
+    } else{
+        name.classList.remove('is-invalid');
+    }
+}
+
+
+function validateZip() {
+    const zip = document.getElementById('zip');
+    const re = /^[0-9]{4}$/;
+
+    if(!re.test(zip.value)){
+        zip.classList.add('is-invalid');
+    } else{
+        zip.classList.remove('is-invalid');
+    }
+}
+
+
+function validateEmail() {
+    const email = document.getElementById('email');
+    const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+    if(!re.test(email.value)){
+        email.classList.add('is-invalid');
+    } else{
+        email.classList.remove('is-invalid');
+    }
+}
+
+
+function validatePhone() {
+    const phone = document.getElementById('phone');
+    //const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+    const re = /^[+]\d{2}?[- ]?\d{2}?[- ]?\d{3}?[- ]?\d{4}$/;
+
+    if(!re.test(phone.value)){
+        phone.classList.add('is-invalid');
+    } else{
+        phone.classList.remove('is-invalid');
+    }
+}
+
+
+
+
+// Get local json data
+function getJson() {
+    fetch('zip.json')
+      .then(function(res){
+        return res.json();
+      })
+      .then(function(data) {
+        console.log(data);
+        // This is an array so we have to loop through it
+        let output = '';
+  
+        data.forEach(function(post){
+          output += `<li>${post.title}</li>`;
+        });
+        document.getElementById('output').innerHTML = output;
+  
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+  }
